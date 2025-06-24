d4b43eecbe3d7b52aaf3a6a9a1d63703b160c079
Justin Chow <Justin.chow@slalom.com>
2025-06-24 17:43:43 +0000

    created stg files and source in model folder

---
+++ models/stg_customers.sql
@@ -0,0 +1,8 @@
+select
+    id as customer_id,
+    first_name,
+    last_name
+--from raw.jaffle_shop.customers
+
+-- adding the sources in the yaml file creates the graph
+from {{source("jaffle_shop","customers")}}
